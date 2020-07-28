9003df843d0efcdda67b86d7894f8355016ff3cb
apgit33 <adrienpaturot@yahoo.fr>
2020-07-28 20:33:58 +0200

    Update twig + css
    Modification du fichier edit.php pour l'intégrer avec Twig au reste du projet. Modification visuel formulaire

--- produit.php
+++ produit.php
@@ -7,19 +7,13 @@ $twig = new \Twig\Environment($loader, [
     'cache' => false,
 ]);
 
-
 //  Requête sql d'éxtracton d'informations de la base de donnée
-$sql = "SELECT id, localisation, name, reference, categorie, date, guarantee, price, maintenance, picture, manual FROM product";
+$sql = "SELECT id, localisation, name, reference, categorie, date, guarantee, price, maintenance, picture, manual FROM products";
 $sth = $dbh->prepare($sql);
 $sth->execute();
 
 /* Affichage et protection des valeurs extraites*/
 $datas = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-// /* Date mise au format français*/
-// $intlDateFormatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
-
-
 
 $template = $twig->load('produit.html.twig');
 echo $template->render(['liste_produits' => $datas]);
