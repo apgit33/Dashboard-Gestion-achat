3f67a882ecf00f7e2bad73798242c1a5de1675ac
apgit33 <adrienpaturot@yahoo.fr>
2020-08-18 16:35:30 +0200

    Update delete
    Ajout de la suppression des médias lors de la suppression d'un produit

--- produit.php
+++ produit.php
@@ -9,12 +9,23 @@ $twig = new \Twig\Environment($loader, [
 ]);
 $user = (isset($_SESSION['user']))? $_SESSION['user'] : "";
 
-// Vérifie si il peut récupérer un id
+//Suppression du produit dans la BDD et des médias
 if (isset( $_POST['modal_delete'])){
+    $id = $_POST['modal_delete'];
+    //Récupère le nom du ticket du manuel pour supprimer les fichiers correspondant
+    $sth = $dbh->prepare("SELECT picture, manual FROM products WHERE id=:id");
+    $sth->bindParam(':id', $id, PDO::PARAM_INT);
+    $sth->execute();
+    $datas = $sth->fetch(PDO::FETCH_ASSOC);
+    $tmp_ticket = $datas['picture'];
+    $tmp_manual = $datas['manual'];
+    unlink("./medias/ticket_achat/$tmp_ticket");
+    if($tmp_manual !== "") {unlink("./medias/manual/$tmp_manual");}
+    
     // Requête sql de supression
     $sql = 'DELETE FROM products WHERE id=:id';
     $sth = $dbh->prepare($sql);
-    $sth->bindParam(':id', $_POST['modal_delete'], PDO::PARAM_INT);
+    $sth->bindParam(':id', $id, PDO::PARAM_INT);
     $sth->execute();
 } 
 
@@ -35,7 +46,7 @@ $first_article_page = $limit + 1;
 $last_article_page = $limit + $nombre;
 
 //  Requête sql d'éxtracton d'informations de la base de donnée
-$sql = "SELECT products.id, localisation, name, reference,  categorie.nom as categorie, date, guarantee, price, maintenance, picture, manual,adresse FROM products INNER JOIN categorie ON categorie.id = products.categorie LIMIT $limit,$nombre";
+$sql = "SELECT products.id, localisation, name, reference,  categorie.nom as categorie, date, guarantee, price, maintenance, picture, manual,adresse FROM products INNER JOIN categorie ON categorie.id = products.categorie ORDER BY `products`.`id` DESC LIMIT $limit,$nombre";
 $sth = $dbh->prepare($sql);
 $sth->execute();
 
