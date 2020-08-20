24648dc42d8157f14371c6f53c55ab2bc28558ab
apgit33 <adrienpaturot@yahoo.fr>
2020-08-20 16:46:45 +0200

    Major fix
    Problème de référence fixé, ajout du changements du nom des médias si on update la réf et amélioration sql

--- treatement/edit_ajout.php
+++ treatement/edit_ajout.php
@@ -3,12 +3,9 @@ session_start();
 require_once "../db.php";
 
 // initialisation/récupération des variables
+$flag=false;
 $validation = false;
 $erreurs[]= "";
-$ticket = "";
-$manual = "";
-$valide_ticket = false;
-$valide_manual = false;
 $path_ticket = "";
 $extensionUpload_ticket = "";
 $extensionUpload_manual = "";
@@ -24,25 +21,31 @@ $guarantee = (isset($_POST['date_guarantee']) ? trim($_POST['date_guarantee']):"
 $price = (isset($_POST['price']) ? trim($_POST['price']):"");
 $maintenance = (isset($_POST['maintenance']) ? trim($_POST['maintenance']):"");
 $new_categorie = (isset($_POST['new_categorie']) ? trim($_POST['new_categorie']):"");
+$ticket = (isset($_POST['ticket']) ? trim($_POST['ticket']):"");
+$manual = (isset($_POST['manual']) ? trim($_POST['manual']):"");
 
 /* **************
 Début des tests 
 ************** */
 //nom
-if(!preg_match("/^[\p{L}-]*$/u",$name)) {
-    $erreurs[]["name"] = "Nom incorrect";
-}elseif (strlen($name) < 3 || strlen($name) > 45) {
+if (strlen($name) < 3 || strlen($name) > 45) {
     $erreurs[]["name"] = "Entre 3 et 45 caractères autorisé";
 }
 
 //référence
-if(!preg_match("/^[\p{L}\p{N}_.-]*$/u",$reference)) {
+if(!preg_match("/^[\p{L}\p{N}-]*$/u",$reference)) {
     $erreurs[]["reference"] = "Référence incorrecte";
 }elseif (strlen($reference) !== 6){
     $erreurs[]["reference"] = "Taille non autorisée (6 caractères)";
 }else{
-    //teste si la référence est déjà présente dans la BDD
-    $sth = $dbh->prepare("SELECT id FROM products WHERE `reference`= :ref");
+    //teste si la référence est déjà présente dans la BDD en omettant sa ref de base en edition
+    if(isset($_POST['id']) && $_POST['id'] !== "") {
+        $flag = true;
+        $sth = $dbh->prepare("SELECT reference FROM products WHERE `reference`= :ref AND NOT `id`= :id");
+        $sth->bindParam(":id", $_POST['id'], PDO::PARAM_STR);
+    }else {
+        $sth = $dbh->prepare("SELECT reference FROM products WHERE `reference`= :ref");
+    }
     $sth->bindParam(":ref", $reference, PDO::PARAM_STR);
     $sth->execute();
     if ($sth->fetch(PDO::FETCH_ASSOC)) {
@@ -57,11 +60,11 @@ if($localisation == "") {
 
 //adresse
 if(strlen($adresse) > 45) {
-    $erreurs[]['adresse'] = "45 caractères maximum";
+    $erreurs[]['adresse'] = "45 caractères maximum : ".strlen($adresse)." actuel(s)";
 }
 
 //catégorie
-if(($categorie == -1 && ( $new_categorie =="" || !preg_match("/^[\p{L}\p{N}_.-]*$/u",$new_categorie))) || ($categorie==0)){
+if(($categorie == -1 && ( $new_categorie =="" )) || ($categorie==0)){
     $erreurs[]["categorie"] = "Catégorie incorrecte";
 }else{
     //teste si la catégorie est déjà présente dans la BDD
@@ -86,24 +89,24 @@ if(!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$guaran
 }
 
 //prix
-if(!preg_match("/^-?(?:\d+|\d*\.\d+)$/",$price)) {
+if(!preg_match("/^([0-9]{1,3}){1}(\.[0-9]{1,2})?$/",$price)) {
     $erreurs[]["price"] = "Prix incorrect";
+}elseif ($price == 0) {
+    $erreurs[]["price"] = "Entre 0 et 1000€ non inclu";
 }
 
 //maintenance
-if(strlen($maintenance) < 5 || strlen($maintenance) >= 255){ 
+if(strlen($maintenance) < 5 || strlen($maintenance) > 255){ 
     $erreurs[]['maintenance'] = "Entre 5 et 255 caractères seulement : ".strlen($maintenance)." actuel";
 }
 
 //ticket d'achat
-if(isset($_FILES['ticket_file']) && !empty($_FILES['ticket_file']['name'])){
+if(isset($_FILES['ticket']) && !empty($_FILES['ticket']['name'])){
     $maxsize = 2097152;
     $extensions = array('jpg', 'jpeg', 'png');
-    if($_FILES['ticket_file']['size'] <= $maxsize){
-        $extensionUpload_ticket = strtolower(substr(strrchr($_FILES['ticket_file']['name'], '.'), 1));
-        if(in_array($extensionUpload_ticket, $extensions)){
-            $valide_ticket = true; //création des fichiers plus bas (avant insertion sql)
-        }else{
+    if($_FILES['ticket']['size'] <= $maxsize){
+        $extensionUpload_ticket = strtolower(substr(strrchr($_FILES['ticket']['name'], '.'), 1));
+        if(!in_array($extensionUpload_ticket, $extensions)){ //création des fichiers plus bas (avant insertion sql)
             $erreurs[]['ticket'] = "L'image n'est pas au bon format!";
         }
     }
@@ -115,14 +118,12 @@ if(isset($_FILES['ticket_file']) && !empty($_FILES['ticket_file']['name'])){
 }
 
 //manuel
-if(isset($_FILES['manual_file']) && !empty($_FILES['manual_file']['name'])){
+if(isset($_FILES['manual']) && !empty($_FILES['manual']['name'])){
     $maxsize = 20971520;
     $extensions = array('pdf');
-    if($_FILES['manual_file']['size'] <= $maxsize){
-        $extensionUpload_manual = strtolower(substr(strrchr($_FILES['manual_file']['name'], '.'), 1));
-        if(in_array($extensionUpload_manual, $extensions)){
-            $valide_manual = true; //création des fichiers plus bas (avant insertion sql)
-        }else{
+    if($_FILES['manual']['size'] <= $maxsize){
+        $extensionUpload_manual = strtolower(substr(strrchr($_FILES['manual']['name'], '.'), 1));
+        if(!in_array($extensionUpload_manual, $extensions)){//création des fichiers plus bas (avant insertion sql)
             $erreurs[]['manual'] = "Le fichier n'est pas au bon format!";
         }
     }
@@ -145,13 +146,30 @@ if (!isset($erreurs[1])) {
     $validation = true;
 
     //on crée les fichiers ici pour éviter d'en créer un à chaque erreur
-    if(move_uploaded_file($_FILES['ticket_file']['tmp_name'], "../medias/ticket_achat/$reference.$extensionUpload_ticket")){
+    if(move_uploaded_file($_FILES['ticket']['tmp_name'], "../medias/ticket_achat/$reference.$extensionUpload_ticket")){
         $ticket = "$reference.$extensionUpload_ticket";
     }
-    if(move_uploaded_file($_FILES['manual_file']['tmp_name'], "../medias/manual/$reference.$extensionUpload_manual")){
+    if(move_uploaded_file($_FILES['manual']['tmp_name'], "../medias/manual/$reference.$extensionUpload_manual")){
         $manual = "$reference.$extensionUpload_manual";
     }
 
+    //si on a changé la ref du produit on rename les médias
+    if($flag===true) {
+        $sth = $dbh->prepare("SELECT ticket,manual FROM products WHERE id=:id");
+        $sth->bindParam(':id', $_POST['id'], PDO::PARAM_STR);
+        $sth->execute();
+        $data = $sth->fetch(PDO::FETCH_ASSOC);
+        $ticket = $data['ticket'];
+        $manual = $data['manual'];
+        if ($ticket!="") {
+            rename("../medias/ticket_achat/$ticket","../medias/ticket_achat/$reference.".explode(".",$ticket)[1]);
+            $ticket = "$reference.".explode(".",$ticket)[1];
+        }
+        if ($manual!="") {
+            rename("../medias/manual/$manual","../medias/manual/$reference.".explode(".",$manual)[1]);
+            $manual = "$reference.".explode(".",$manual)[1];
+        }
+    }
     //si une nouvelle catégorie est crée, on l'insere dans la BDD
     if ($new_categorie != ""){
         $sth = $dbh->prepare("INSERT INTO categorie(nom) VALUES (:nom)");
@@ -159,19 +177,10 @@ if (!isset($erreurs[1])) {
         $sth->execute();
         $categorie = $dbh->lastInsertId(); //on récupère l'id de la nouvelle catégorie
     }
-
     if(isset($_POST['id']) && $_POST['id'] !== "") {
-        if (($ticket =="") && ($manual =="")){
-            $sql = 'UPDATE products SET categorie=:categorie,localisation=:localisation, name=:name, reference=:reference,adresse=:adresse, date=:date, guarantee=:guarantee, price=:price, maintenance=:maintenance  WHERE id=:id';
-        }elseif($ticket =="") {
-            $sql = 'UPDATE products SET categorie=:categorie,manual=:manual, localisation=:localisation, name=:name, reference=:reference,adresse=:adresse, date=:date, guarantee=:guarantee, price=:price, maintenance=:maintenance  WHERE id=:id';
-        }elseif($manual==""){
-            $sql = 'UPDATE products SET categorie=:categorie,picture=:picture, localisation=:localisation, name=:name, reference=:reference,adresse=:adresse, date=:date, guarantee=:guarantee, price=:price, maintenance=:maintenance  WHERE id=:id';
-        }else{
-            $sql = 'UPDATE products SET categorie=:categorie,picture=:picture,manual=:manual,localisation=:localisation, name=:name, reference=:reference,adresse=:adresse, date=:date, guarantee=:guarantee, price=:price, maintenance=:maintenance  WHERE id=:id';
-        }
+        $sql = 'UPDATE products SET categorie=:categorie,ticket=:ticket,manual=:manual,localisation=:localisation, name=:name, reference=:reference,adresse=:adresse, date=:date, guarantee=:guarantee, price=:price, maintenance=:maintenance  WHERE id=:id';
     }else {
-        $sql = "INSERT INTO products(categorie,localisation,name,reference,adresse,date,guarantee,price,maintenance,picture,manual) VALUES(:categorie,:localisation,:name,:reference,:adresse,:date,:guarantee,:price,:maintenance,:picture,:manual)";
+        $sql = "INSERT INTO products(categorie,localisation,name,reference,adresse,date,guarantee,price,maintenance,ticket,manual) VALUES(:categorie,:localisation,:name,:reference,:adresse,:date,:guarantee,:price,:maintenance,:ticket,:manual)";
     }
     // Préparation, protection et execution de la requête sql
     $sth = $dbh->prepare($sql);
@@ -185,12 +194,14 @@ if (!isset($erreurs[1])) {
     $sth->bindValue(':guarantee', strftime("%Y-%m-%d", strtotime($guarantee)), PDO::PARAM_STR);
     $sth->bindParam(':price', $price, PDO::PARAM_STR);
     $sth->bindParam(':maintenance', $maintenance, PDO::PARAM_STR);
-    if ($ticket !=""){$sth->bindParam(':picture', $ticket, PDO::PARAM_STR);}
-   $sth->bindParam(':manual', $manual, PDO::PARAM_STR);
+    $sth->bindParam(':ticket', $ticket, PDO::PARAM_STR);
+    $sth->bindParam(':manual', $manual, PDO::PARAM_STR);
     if(isset($_POST['id']) && $_POST['id'] !== "") {
         $sth->bindParam('id', $id, PDO::PARAM_INT);
     }
+
     $sth->execute();
+
 }
 
 echo json_encode(array('validation' => $validation,'erreurs' => $erreurs));
